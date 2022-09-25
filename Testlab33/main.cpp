c1a57c82473cdb4e19c2b727068d8657ab178ab2
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-09-25 16:02:12 +0300

    Add project files.

---
+++ Testlab33/main.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+// Lab_03_3.cpp
+// < Кулявця Владислава Романовича >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 012
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= -6)
+		y = R;
+	else
+		if (-6 < x && x <= -R)
+			y = (R + x) / (-R + 6);
+		else
+			if (-R < x && x <= 0)
+				y = R - sqrt(R * R - (x + R) * (x + R));
+			else
+				if (0 < x && x <= R)
+					y = sqrt(R * R - x * x);
+				else
+					y = -(x - R);
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
